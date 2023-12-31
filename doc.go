0a3392861dca2486b875c9b4f3081885cace66db
Kirill <kirillsafin@bk.ru>
2024-01-01 01:42:36 +0300

    add package docs

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package itertools provides Iterator type to iteratively access
+// elements of collections/sequences and many functions and methods to works
+// with iterators.
+package itertools
---
+++ erroriter/doc.go
@@ -0,0 +1,3 @@
+// Package erroriter provides ErrorIterator type
+// and methods to work with iterations possibly containing errors.
+package erroriter
